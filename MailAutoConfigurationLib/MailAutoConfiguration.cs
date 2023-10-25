27976cee657438ce49dd4f8ea663f5807c986a72
Y.Arimitsu <yarimit@gmail.com>
2023-10-25 20:19:40 +0900

    プロジェクト ファイルを追加します。

---
+++ MailAutoConfigurationLib/MailAutoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using MailAutoConfigurationLib.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MailAutoConfigurationLib
+{
+    public class MailAutoConfiguration
+    {
+        public ConfigFileFormat? ParseXML(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ConfigFileFormat));
+
+            using (var reader = new StringReader(xml))
+            {
+                return serializer.Deserialize(reader) as ConfigFileFormat;
+            }
+
+        }
+    }
+}
---
+++ MailAutoConfigurationLib/Models/ConfigFileFormat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MailAutoConfigurationLib.Models
+{
+    /// <summary>
+    /// https://wiki.mozilla.org/Thunderbird:Autoconfiguration:ConfigFileFormat
+    /// </summary>
+    [XmlRoot("clientConfig")]
+    public class ConfigFileFormat
+    {
+        [XmlElement("emailProvider")]
+        public List<EmailProvider> EmailProviders { get; set; } = new List<EmailProvider>();
+    }
+
+    public class EmailProvider
+    {
+        [XmlElement("displayName")]
+        public string? DisplayName { get; set; }
+        [XmlElement("displayShortName")]
+        public string? DisplayShortName { get; set; }
+
+        [XmlElement("domain")]
+        public List<string> Domains { get; set; } = new List<string>();
+
+
+        [XmlElement("incomingServer")]
+        public IncomingServer? IncomingServer { get; set; }
+
+        [XmlElement("outgoingServer")]
+        public OutgoingServer? OutgoingServer { get; set; }
+    }
+
+    public class IncomingServer
+    {
+
+    }
+
+    public class OutgoingServer
+    {
+
+    }
+
+}
